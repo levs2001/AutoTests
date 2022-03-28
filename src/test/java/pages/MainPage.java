bf9f58be896ee0e38ab3fd75dbebb1734c8ec5ba
levs2 <levs2@DESKTOP-LSHB5BS>
2022-03-28 20:15:56 +0300

    correct names

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,12 +1,11 @@
 package pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class MainPage extends PresentableObject {
-    private static final By CHANGE_LANG_BTN_XPATH = By.xpath("//*[contains(@class, 'ph-lang-select')]");
-    private static final By SETTINGS_BTN_XPATH = By.xpath("//*[@id = 'hook_Block_LeftColumnTopCardUser']//*[@href = '/settings']/*[@class = 'tico']");
+    private static final By CHANGE_LANG_BTN_BY = By.xpath("//*[contains(@class, 'ph-lang-select')]");
+    private static final By SETTINGS_BTN_BY = By.xpath("//*[@id = 'hook_Block_LeftColumnTopCardUser']//*[@href = '/settings']/*[@class = 'tico']");
 
 
     @Override
@@ -19,11 +18,11 @@ public class MainPage extends PresentableObject {
     }
 
     public ChangeLanguagePageElement openChangeLanguageModal() {
-        webDriver.findElement(CHANGE_LANG_BTN_XPATH).click();
+        webDriver.findElement(CHANGE_LANG_BTN_BY).click();
         return new ChangeLanguagePageElement(webDriver);
     }
 
     public String getSettingsText() {
-        return webDriver.findElement(SETTINGS_BTN_XPATH).getText();
+        return webDriver.findElement(SETTINGS_BTN_BY).getText();
     }
 }
